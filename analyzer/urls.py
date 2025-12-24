aa3d90fb8055619c3ebe1a2952f6dd80a4ef7e56
danisiomo <darkhand860@gmail.com>
2025-12-24 20:18:26 +0500

    Initial commit: базовая структура проекта Django
    - Создана структура проекта
    - Настроены основные приложения (analyzer, users)
    - Созданы базовые шаблоны
    - Добавлены TZ.md и README.md
    - Настроены основные маршруты

---
+++ analyzer/urls.py
@@ -0,0 +1,10 @@
+# analyzer/urls.py
+from django.urls import path
+from . import views
+
+app_name = 'analyzer'
+
+urlpatterns = [
+    # Пока оставим пустым, добавим маршруты позже
+    # path('dashboard/', views.dashboard, name='dashboard'),
+]
---
+++ config/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('users/', include('users.urls')),
+    path('analyzer/', include('analyzer.urls')),
+    #path('api/', include('api.urls')),  # если создавали api app
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
