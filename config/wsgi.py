9c92b621a69616a1acd1883e08546a840656e7f1
danisiomo <darkhand860@gmail.com>
2025-12-30 12:55:39 +0500

    Чистка проекта для деплоя

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,16 +1,19 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# Путь к проекту
+path = '/home/danisiomo/rent_analyzer'
+if path not in sys.path:
+    sys.path.append(path)
 
+# Активируем виртуальное окружение
+activate_this = '/home/danisiomo/rent_analyzer/venv/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
+
+# Настройки Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
+# Запуск приложения
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
