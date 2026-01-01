46272e47cd1c622e4172d63adee98cdcfffcb54d
danisiomo <daniil.kaptiev@urfu.me>
2026-01-01 20:51:15 +0500

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,6 @@ app_name = 'users'
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    path('logout/', views.custom_logout, name='logout'),
     path('register/', views.register, name='register'),
 ]
