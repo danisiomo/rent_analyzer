91085015dda6f7c5ff62e1a4e8194682e72f9908
danisiomo <daniil.kaptiev@urfu.me>
2026-01-01 21:13:54 +0500

    Update views.py

--- analyzer/views.py
+++ analyzer/views.py
@@ -1,4 +1,4 @@
-import report
+#import report
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView
