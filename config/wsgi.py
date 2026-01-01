5d019caf56ccc88e9179f483847aaa21d986df8c
danisiomo <daniil.kaptiev@urfu.me>
2026-01-01 17:14:08 +0500

    Исправление багов

--- config/wsgi.py
+++ config/wsgi.py
@@ -7,9 +7,9 @@ if path not in sys.path:
     sys.path.append(path)
 
 # Активируем виртуальное окружение
-activate_this = '/home/danisiomo/rent_analyzer/venv/bin/activate_this.py'
-with open(activate_this) as file_:
-    exec(file_.read(), dict(__file__=activate_this))
+#activate_this = '/home/danisiomo/rent_analyzer/venv/bin/activate_this.py'
+#with open(activate_this) as file_:
+#    exec(file_.read(), dict(__file__=activate_this))
 
 # Настройки Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
